2046427f2c80ba973c605de3269a73bc8334aaed
Akash Badhe <akashb@arista.com>
2025-06-30 12:41:37 +0530

    WIP: push code completed so far

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RetirementForm from './components/RetirementForm';
+import RetirementForm from './components/retirement-planner/RetirementForm';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
